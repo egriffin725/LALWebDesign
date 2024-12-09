2825aa73bd611475044b56d397836ecbe1183b61
lindseyW111 <144359342+lindseyW111@users.noreply.github.com>
2024-12-09 16:02:59 -0500

    Add files via upload

---
+++ LAL/index.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+
+  var siteWidth = 1280;
+  var scale = screen.width /siteWidth;
+
+  document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
